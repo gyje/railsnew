5aff7d49fa9db61c162d3f52e6eae562f495a9a8
pushring0 <gyj37191@gmail.com>
2017-01-24 15:00:29 +0800

    change sp_controller.rb

--- rails3a/test/controllers/sp_controller_test.rb
+++ rails3a/test/controllers/sp_controller_test.rb
@@ -3,25 +3,25 @@ require 'test_helper'
 class SpControllerTest < ActionDispatch::IntegrationTest
 
   def setup
-      @base_title="pages"
+      @title="pages"
   end
 
   test "should get about" do
     get sp_about_url
     assert_response:success
-    assert_select "title" , "about,#{@base_title}"
+    assert_select "title" , "about,#{@title}"
   end
 
   test "should get home" do
     get sp_home_url
     assert_response :success
-    assert_select "title" ,"home,#{@base_title}"
+    assert_select "title" ,"home,#{@title}"
   end
 
   test "should get help" do
     get sp_help_url
     assert_response :success
-    assert_select "title" , "help,#{@base_title}"
+    assert_select "title" , "help,#{@title}"
   end
 
 end
