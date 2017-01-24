9ab389879f09098d19d966934728b318275b3c3c
pushring0 <gyj37191@gmail.com>
2017-01-24 13:18:57 +0800

    add test

--- rails3a/test/controllers/sp_controller_test.rb
+++ rails3a/test/controllers/sp_controller_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class SpControllerTest < ActionDispatch::IntegrationTest
+  test "should get about" do
+    get sp_about_url
+    assert_response:success
+  end
+
   test "should get home" do
     get sp_home_url
     assert_response :success
