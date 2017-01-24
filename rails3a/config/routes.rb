f5fc81346cd4b28197d71a151199325bb1f2c20c
pushring0 <gyj37191@gmail.com>
2017-01-24 12:51:07 +0800

    rails generate controller sp home help

--- rails3a/config/routes.rb
+++ rails3a/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'sp/home'
+
+  get 'sp/help'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "application#hello"
 end
