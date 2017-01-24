7fa29121c7150092d2db7ecf3f66b1e2c6e083ad
pushring0 <gyj37191@gmail.com>
2017-01-24 14:15:13 +0800

    @base_title

--- rails3a/test/controllers/sp_controller_test.rb
+++ rails3a/test/controllers/sp_controller_test.rb
@@ -1,19 +1,27 @@
 require 'test_helper'
 
 class SpControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+      @base_title="pages"
+  end
+
   test "should get about" do
     get sp_about_url
     assert_response:success
+    assert_select "title" , "about,#{@base_title}"
   end
 
   test "should get home" do
     get sp_home_url
     assert_response :success
+    assert_select "title" ,"home,#{@base_title}"
   end
 
   test "should get help" do
     get sp_help_url
     assert_response :success
+    assert_select "title" , "help,#{@base_title}"
   end
 
 end
